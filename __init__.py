1ec5fc9ab98a1ee90c84623b33fdcbc083ea3f58
MatthijsdeJ <matthijsdj@live.nl>
2022-01-30 20:22:29 +0100

    Removed unnecessary __init__ file.

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import metrics
-import imitation_data_loading
-import grid2op_data_loading
-import util
-from util import get_config
-import Tutor.Tutor
-import Tutor.Generate_teaching_dataset
-import action_space.generate_action_space
